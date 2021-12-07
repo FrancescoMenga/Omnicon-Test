89c738d698eeed2af4e32264cd28d9ed11c0320d
FrancescoMenga <95043931+FrancescoMenga@users.noreply.github.com>
2021-12-07 15:27:43 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,16 @@
-L.TileLayer.Provider = L.TileLayer.extend({
+<script>
+            (function(root, factory) {
+                if (typeof define === 'function' && define.amd) {
+                    define(['leaflet'], factory);
+                } else if (typeof modules === 'object' && module.exports) {
+                    module.exports = factory(require('leaflet'));
+                } else {
+                    factory(L);
+                }
+            }(this, function(L) {
+                'use strict';
+        
+                L.TileLayer.Provider = L.TileLayer.extend({
                     initialize: function(arg, options) {
                         var providers = L.TileLayer.Provider.providers;
                         var parts = arg.split('.');
@@ -35,3 +47,87 @@ L.TileLayer.Provider = L.TileLayer.extend({
                         L.TileLayer.prototype.initialize.call(this, provider.url, layerOpts);
                     }
                 });
+
+                L.TileLayer.Provider.providers = {
+                OpenStreetMap: {
+                    url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                    options: {
+                        minZoom: 3,
+                        maxZoom: 19,
+                        attribution:
+                            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    },
+                    variants: {
+                        Mapnik: {},
+                        DE: {url: 'https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png',options: {maxZoom: 18}},
+                    }
+                },
+                GoogleMaps: {
+                    url: 'http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',
+                    options: {
+                        minZoom: 3,
+                        maxZoom: 19,
+                        subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+                        attribution:'&copy; <a href="https://about.google/brand-resource-center/products-and-services/geo-guidelines/">Google Maps</a>'
+                    },
+                    variants: {
+                        Streets: {
+                            url: 'http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',
+                            options: {maxZoom: 19}
+                        },
+                        Hybrid: {
+                            url: 'http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}',
+                            options: {maxZoom: 19}
+                        },
+                        Satellite: {
+                            url: 'http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',
+                            options: {maxZoom: 19}
+                        },
+
+                    }
+                },
+                Stamen: {
+                    url: 'https://stamen-tiles-{s}.a.ssl.fastly.net/{variant}/{z}/{x}/{y}{r}.{ext}',
+                    options: {
+                        attribution:
+                            'Map tiles by <a href="http://stamen.com">Stamen Design</a>, ' +
+                            '<a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; ' +
+                            'Map data {attribution.OpenStreetMap}',
+                        subdomains: 'abcd',
+                        minZoom: 0,
+                        maxZoom: 20,
+                        variant: 'toner',
+                        ext: 'png'
+                    },
+                    variants: {Toner: 'toner',}
+                },
+                OpenWeatherMap: {
+                    url: 'http://{s}.tile.openweathermap.org/map/{variant}/{z}/{x}/{y}.png?appid={apiKey}',
+                    options: {
+                        minZoom: 3,
+                        maxZoom: 19,
+                        attribution: 'Map data &copy; <a href="http://openweathermap.org">OpenWeatherMap</a>',
+                        apiKey: '0e1e87f7dad2414965f76a3c9bf1f892',
+                        opacity: 0.5
+                    },
+                    variants: {
+                        Clouds: 'clouds',
+                        CloudsClassic: 'clouds_cls',
+                        Precipitation: 'precipitation',
+                        PrecipitationClassic: 'precipitation_cls',
+                        Rain: 'rain',
+                        RainClassic: 'rain_cls',
+                        Pressure: 'pressure',
+                        PressureContour: 'pressure_cntr',
+                        Wind: 'wind',
+                        Temperature: 'temp',
+                        Snow: 'snow'
+                    }
+                },
+            };
+                L.tileLayer.provider = function(provider, options) {
+                    return new L.TileLayer.Provider(provider, options);
+                };
+                return L;
+            }));
+        </script>
